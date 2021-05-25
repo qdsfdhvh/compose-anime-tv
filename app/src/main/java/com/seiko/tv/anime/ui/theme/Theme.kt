c98771fefd557bfed8a4c39bdf555ff9db8b5c99
seiko <605590140@qq.com>
2021-05-25 22:02:46 +0800

    Add compose color library 'holi'

--- app/src/main/java/com/seiko/tv/anime/ui/theme/Theme.kt
+++ app/src/main/java/com/seiko/tv/anime/ui/theme/Theme.kt
@@ -5,17 +5,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.siddroid.holi.colors.CoolColor
+import com.siddroid.holi.colors.MaterialColor
 
 private val DarkColorPalette = darkColors(
-  primary = Purple200,
-  primaryVariant = Purple700,
-  secondary = Teal200
+  primary = CoolColor.DARK_GREY,
+  primaryVariant = CoolColor.BLACK,
+  secondary = CoolColor.BLUE
 )
 
 private val LightColorPalette = lightColors(
-  primary = Purple500,
-  primaryVariant = Purple700,
-  secondary = Teal200
+  primary = MaterialColor.PINK_200,
+  primaryVariant = MaterialColor.PINK_700,
+  secondary = MaterialColor.RED_300
 
   /* Other default colors to override
     background = Color.White,
