842894605f78d88863e76e41dc9166d851502014
seiko <605590140@qq.com>
2021-08-01 21:42:43 +0800

    clear useless code in HomeScene

--- app/src/main/kotlin/com/seiko/tv/anime/ui/home/HomeScene.kt
+++ app/src/main/kotlin/com/seiko/tv/anime/ui/home/HomeScene.kt
@@ -1,31 +1,21 @@
 package com.seiko.tv.anime.ui.home
 
-import androidx.compose.animation.core.animateIntSizeAsState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.insets.statusBarsPadding
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.seiko.tv.anime.model.Anime
-import com.seiko.tv.anime.ui.widget.foundation.NetworkImage
 import com.seiko.tv.anime.ui.widget.foundation.TvTabBar
 import com.seiko.tv.anime.ui.widget.foundation.TvTitleGroup
-import com.seiko.tv.anime.util.extensions.focusTarget
 
 @Composable
 fun HomeScene() {
@@ -57,50 +47,5 @@ fun HomeScene() {
         )
       }
     }
-  }
-}
-
-@Composable
-private fun AnimeCard(index: Int, anime: Anime) {
-  val focusRequester = remember { FocusRequester() }
-  var isFocused by remember { mutableStateOf(index == 0) }
-
-  val size by animateIntSizeAsState(
-    if (isFocused) IntSize(160, 240) else IntSize(140, 200),
-  )
-
-  Box(
-    modifier = Modifier
-      .focusTarget(focusRequester) {
-        isFocused = it.isFocused
-      }
-      .size(160.dp, 280.dp),
-  ) {
-    Column(
-      modifier = Modifier.align(Alignment.Center)
-    ) {
-      NetworkImage(
-        data = anime.imageUrl,
-        modifier = Modifier
-          .clip(RoundedCornerShape(6.dp))
-          .size(size.width.dp, size.height.dp)
-      )
-      Spacer(modifier = Modifier.height(6.dp))
-      Text(
-        text = anime.title,
-        modifier = Modifier
-          .width(140.dp)
-          .align(Alignment.CenterHorizontally),
-        color = if (isFocused) Color.White else Color.Black,
-        fontSize = 16.sp,
-        textAlign = TextAlign.Center,
-        maxLines = 1,
-      )
-    }
-  }
-
-  DisposableEffect(Unit) {
-    if (index == 0) focusRequester.requestFocus()
-    onDispose { }
   }
 }
