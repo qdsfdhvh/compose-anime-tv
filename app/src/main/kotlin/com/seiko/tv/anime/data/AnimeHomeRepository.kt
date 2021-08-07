79f150a0a3e6c7ccfb606f30dab034782e5655b3
seiko <605590140@qq.com>
2021-08-07 23:41:02 +0800

    remove OptIn in AnimeHomeRepository

--- app/src/main/kotlin/com/seiko/tv/anime/data/AnimeHomeRepository.kt
+++ app/src/main/kotlin/com/seiko/tv/anime/data/AnimeHomeRepository.kt
@@ -4,16 +4,12 @@ import com.seiko.tv.anime.http.YhdmService
 import com.seiko.tv.anime.model.Anime
 import com.seiko.tv.anime.model.AnimeGroup
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class AnimeHomeRepository @Inject constructor(private val service: YhdmService) {
-
-  @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
   fun getAnimeList(): Flow<List<AnimeGroup>> {
     return flow {
       val response = service.getHomeResponse()
