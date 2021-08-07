946d8f14e2c37de7ae8faeac4317ca35b5b1fb6c
seiko <605590140@qq.com>
2021-08-07 23:41:16 +0800

    draw DetailScene

---
+++ app/src/main/kotlin/com/seiko/tv/anime/component/foundation/TvEpisodeList.kt
@@ -0,0 +1,97 @@
+package com.seiko.tv.anime.component.foundation
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.seiko.compose.focuskit.*
+import com.seiko.tv.anime.model.AnimeEpisode
+import com.seiko.tv.anime.ui.theme.AnimeTvTheme
+import com.seiko.tv.anime.ui.theme.backgroundColor
+
+@Composable
+fun TvEpisodeList(
+  title: String,
+  list: List<AnimeEpisode>,
+  parent: ContainerTvFocusItem? = null
+) {
+  val container = parent ?: rememberContainerTvFocusItem()
+
+  Column {
+    Text(
+      text = title,
+      style = MaterialTheme.typography.h6,
+      modifier = Modifier
+        .padding(start = 15.dp, top = 10.dp),
+    )
+    TvLazyRow(container) {
+      itemsIndexed(list) { index, item ->
+        val focusItem = rememberTvFocusItem(
+          key = title + index,
+          container = container,
+          index = index
+        )
+
+        var isFocused by remember { mutableStateOf(false) }
+        EpisodeItem(
+          modifier = Modifier
+            .onTvFocusChanged(focusItem) {
+              isFocused = it.isFocused
+            },
+          episode = item,
+          isFocused = isFocused,
+        )
+      }
+    }
+  }
+}
+
+@Composable
+private fun EpisodeItem(
+  episode: AnimeEpisode,
+  isFocused: Boolean,
+  modifier: Modifier = Modifier,
+) {
+  val scale by animateFloatAsState(if (isFocused) 1.2f else 1f)
+  Text(
+    text = episode.title,
+    color = MaterialTheme.colors.onSurface,
+    modifier = modifier
+      .scale(scale)
+      .padding(10.dp)
+      .shadow(if (isFocused) 5.dp else 0.dp, RoundedCornerShape(4.dp))
+      .background(MaterialTheme.colors.surface, RoundedCornerShape(4.dp))
+      .padding(horizontal = 20.dp, vertical = 10.dp),
+  )
+}
+
+@Preview
+@Composable
+fun EpisodeItemPreview() {
+  AnimeTvTheme {
+    Surface(color = backgroundColor) {
+      Row(modifier = Modifier.padding(5.dp)) {
+        EpisodeItem(
+          episode = AnimeEpisode("第01集", ""),
+          isFocused = true
+        )
+        EpisodeItem(
+          episode = AnimeEpisode("第02集", ""),
+          isFocused = false
+        )
+      }
+    }
+  }
+}
--- app/src/main/kotlin/com/seiko/tv/anime/ui/detail/DetailScene.kt
+++ app/src/main/kotlin/com/seiko/tv/anime/ui/detail/DetailScene.kt
@@ -1,26 +1,20 @@
 package com.seiko.tv.anime.ui.detail
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.statusBarsPadding
 import com.seiko.compose.focuskit.TvLazyColumn
 import com.seiko.compose.focuskit.rememberContainerTvFocusItem
-import com.seiko.tv.anime.component.foundation.NetworkImage
+import com.seiko.tv.anime.component.foundation.TvEpisodeList
+import com.seiko.tv.anime.component.foundation.TvMovieInfo
+import com.seiko.tv.anime.component.foundation.TvTitleGroup
 import com.seiko.tv.anime.di.assisted.assistedViewModel
-import com.seiko.tv.anime.ui.theme.AnimeTvTheme
 
 @Composable
 fun DetailScene(id: Int) {
@@ -35,10 +29,12 @@ fun DetailScene(id: Int) {
   Surface(color = MaterialTheme.colors.background) {
     TvLazyColumn(
       container = container,
-      modifier = Modifier.fillMaxSize(),
+      modifier = Modifier
+        .fillMaxSize()
+        .statusBarsPadding(),
     ) {
       item {
-        TvAnimeInfo(
+        TvMovieInfo(
           title = info.title,
           cover = info.cover,
           releaseTime = info.releaseTime,
@@ -49,92 +45,36 @@ fun DetailScene(id: Int) {
       }
 
       item {
-        TvEpisodeList()
-      }
-    }
-  }
-}
+        val episodeContainer = rememberContainerTvFocusItem(
+          key = Unit,
+          container = container,
+          index = 0
+        )
 
-@Composable
-private fun TvAnimeInfo(
-  modifier: Modifier = Modifier,
-  title: String = "",
-  cover: String = "",
-  releaseTime: String = "",
-  state: String = "",
-  tags: List<String> = emptyList(),
-  description: String = "",
-) {
-  Row {
-    Column(
-      modifier = modifier
-        .weight(0.7f),
-    ) {
-      Text(
-        text = title,
-        style = MaterialTheme.typography.h3,
-      )
-      Spacer(modifier = Modifier.height(20.dp))
-      Row(
-        verticalAlignment = Alignment.CenterVertically,
-      ) {
-        Text(
-          text = releaseTime,
-          style = MaterialTheme.typography.body1
+        TvEpisodeList(
+          title = "播放列表",
+          list = info.episodeList,
+          parent = episodeContainer,
         )
-        Spacer(modifier = Modifier.width(20.dp))
-        Text(
-          text = state,
-          style = MaterialTheme.typography.body1
-        )
-        Spacer(modifier = Modifier.width(20.dp))
-        for (tag in tags) {
-          Text(
-            modifier = Modifier
-              .widthIn(min = 60.dp)
-              .border(1.dp, MaterialTheme.colors.onSurface, RoundedCornerShape(2.dp))
-              .padding(2.dp),
-            text = tag,
-            style = MaterialTheme.typography.body1,
-            textAlign = TextAlign.Center
-          )
-          Spacer(modifier = Modifier.width(10.dp))
+
+        LaunchedEffect(info) {
+          episodeContainer.focusIndex = 0
         }
       }
-      Spacer(modifier = Modifier.height(20.dp))
-      Text(
-        text = description,
-        style = MaterialTheme.typography.subtitle1
-      )
-    }
 
-    NetworkImage(
-      modifier = Modifier
-        .weight(0.3f)
-        .size(200.dp, 300.dp)
-        .background(Color.Blue),
-      data = cover,
-    )
-  }
-}
+      item {
+        val relatedContainer = rememberContainerTvFocusItem(
+          key = Unit,
+          container = container,
+          index = 1
+        )
 
-@Composable
-private fun TvEpisodeList() {
-
-}
-
-@Preview(widthDp = 1280, heightDp = 720)
-@Composable
-private fun TvAnimeInfoPreview() {
-  AnimeTvTheme {
-    Surface(color = MaterialTheme.colors.background) {
-      TvAnimeInfo(
-        title = "小林家的龙女仆 第二季",
-        releaseTime = "2021-07",
-        state = "更新至第5集",
-        tags = listOf("搞笑", "奇幻", "百合", "治愈"),
-        description = "《小林家的龙女仆 第二季》主人公小林是一位女性系统工程师，某天她喝酒喝的很醉兴冲冲的跑到了山上遇到了一头龙，酒醉之下的小林对着龙大倒苦水。听到龙说自己无家可归时，小林趁着酒意开玩笑的说“那你就来我家吧”，之后龙真的就跑到小林的家里，并且还变成了一位女仆——于是一位龙女仆和小林的同居生活就这样开始了……",
-      )
+        TvTitleGroup(
+          parent = relatedContainer,
+          title = "相关推荐",
+          list = info.relatedList
+        )
+      }
     }
   }
 }
