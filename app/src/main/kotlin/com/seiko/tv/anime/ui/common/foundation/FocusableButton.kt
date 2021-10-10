bd91ae7907016a6b5fc33f71b96671b9d33eaca7
seiko <605590140@qq.com>
2021-10-10 16:06:56 +0800

    Add FocusableImageButton & FocusableTextButton

--- app/src/main/kotlin/com/seiko/tv/anime/ui/common/foundation/FocusableButton.kt
+++ app/src/main/kotlin/com/seiko/tv/anime/ui/common/foundation/FocusableButton.kt
@@ -7,12 +7,20 @@ import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import com.seiko.compose.focuskit.TvKeyEvent
 import com.seiko.compose.focuskit.handleTvKey
@@ -47,4 +55,40 @@ fun FocusableButton(
       content(isFocused)
     }
   )
+}
+
+@Composable
+fun FocusableImageButton(
+  image: ImageVector,
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  contentDescription: String? = null,
+  interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+  FocusableButton(
+    modifier = modifier,
+    onClick = onClick,
+    interactionSource = interactionSource
+  ) {
+    Icon(
+      painter = rememberVectorPainter(image),
+      contentDescription = contentDescription
+    )
+  }
+}
+
+@Composable
+fun FocusableTextButton(
+  text: String,
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+  FocusableButton(
+    modifier = modifier,
+    onClick = onClick,
+    interactionSource = interactionSource
+  ) {
+    Text(text)
+  }
 }
