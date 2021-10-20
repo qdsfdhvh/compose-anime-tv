327d5602c707292a93aa9041ca85118471a89b71
seiko <605590140@qq.com>
2021-10-20 22:27:07 +0800

    AGP to 7.1.0-beta01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,6 @@ pluginManagement {
     arrayOf(
       id("com.android.application"),
       id("com.android.library")
-    ).forEach { it version "7.2.0-alpha01" }
+    ).forEach { it version "7.1.0-beta01" }
   }
 }
