249b56d716e474e30c8cc93ccaa676d817c9d6ed
seiko <seiko_des@outlook.com>
2022-07-19 22:09:58 +0800

    set yhdm base url

--- feature/service/src/commonMain/kotlin/com/seiko/tv/anime/Constants.kt
+++ feature/service/src/commonMain/kotlin/com/seiko/tv/anime/Constants.kt
@@ -1,6 +1,6 @@
 package com.seiko.tv.anime
 
-const val YHDM_BAS_URL = "http://www.yhdm.so"
+const val YHDM_BAS_URL = "http://www.yinghuacd.com"
 
 enum class OperatingSystem {
   Android, IOS, Windows, Linux, MacOS, Unknown;
