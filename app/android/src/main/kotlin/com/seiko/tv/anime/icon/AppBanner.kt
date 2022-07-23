0b780e36aeeba98731892eb101a212ae294c032d
seiko <seiko_des@outlook.com>
2022-07-23 10:40:15 +0800

    move icons to android

--- app/android/src/main/kotlin/com/seiko/tv/anime/icon/AppBanner.kt
+++ app/android/src/main/kotlin/com/seiko/tv/anime/icon/AppBanner.kt
@@ -1,13 +1,19 @@
-package com.seiko.tv.anime.ui.theme
+package com.seiko.tv.anime.icon
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.seiko.tv.anime.R
 
 /**
  * 生成 TV Banner
@@ -20,13 +26,12 @@ fun AppBanner() {
       .size(160.dp, 80.dp)
       .background(Color.White)
   ) {
-    // TODO svg to compose
-    // val image = ImageVector.vectorResource(R.drawable.ic_play_station_48dp)
-    // Icon(
-    //   painter = rememberVectorPainter(image = image),
-    //   contentDescription = null,
-    //   modifier = Modifier.align(Alignment.Center)
-    // )
+    val image = ImageVector.vectorResource(R.drawable.ic_play_station_48dp)
+    Icon(
+      painter = rememberVectorPainter(image = image),
+      contentDescription = null,
+      modifier = Modifier.align(Alignment.Center)
+    )
   }
 }
 
