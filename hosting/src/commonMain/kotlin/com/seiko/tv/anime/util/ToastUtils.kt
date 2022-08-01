ff1b33388c3888fb6f1c46a574be7a643b6d863d
seiko <seiko_des@outlook.com>
2022-08-01 15:09:15 +0800

    update ToastUtils

--- hosting/src/commonMain/kotlin/com/seiko/tv/anime/util/ToastUtils.kt
+++ hosting/src/commonMain/kotlin/com/seiko/tv/anime/util/ToastUtils.kt
@@ -3,11 +3,10 @@ package com.seiko.tv.anime.util
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,12 +16,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.filterNot
 
 object ToastUtils {
   fun showToast(msg: String?) {
@@ -40,34 +38,29 @@ fun BoxScope.ToastScreenComponent() {
   var showMsg by remember { mutableStateOf("") }
 
   LaunchedEffect(Unit) {
-    toast.filterNotNull().collect {
-      showMsg = it
+    toast.filterNot { it.isNullOrEmpty() }.collect {
+      showMsg = it!!
       isShown = true
     }
   }
 
   AnimatedVisibility(
     visible = isShown,
-    modifier = Modifier
-      .padding(10.dp)
-      .padding(bottom = 50.dp)
-      .align(Alignment.BottomCenter),
+    modifier = Modifier.padding(bottom = 50.dp).align(Alignment.BottomCenter),
     enter = fadeIn(),
     exit = fadeOut()
   ) {
-    Text(
-      text = showMsg,
-      modifier = Modifier
-        .shadow(1.dp, CircleShape)
-        .background(MaterialTheme.colorScheme.surface, CircleShape)
-        .padding(horizontal = 20.dp, vertical = 10.dp)
-    )
+    Surface(shape = CircleShape, tonalElevation = 1.dp) {
+      Text(showMsg, Modifier.padding(horizontal = 20.dp, vertical = 10.dp))
+    }
   }
 
   if (isShown) {
     LaunchedEffect(Unit) {
       delay(1500)
       isShown = false
+      delay(1500)
+      showMsg = ""
     }
   }
 }
